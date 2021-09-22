216d908c09d4c3b53b2f6b628491a210e885a368
Kunal Pandharkar <63575815+KunalPandharkar@users.noreply.github.com>
2021-09-22 10:46:49 +0530

    Add files via upload

--- Ecommerce/site.js
+++ Ecommerce/site.js
@@ -1,51 +1,71 @@
-let cartcount = localStorage.getItem("cartcount");
+
 const cartvalue = document.getElementById("cartcount");
 let product = {
-    id:null,
+    id: null,
     name: null,
     price: null,
     description: null,
     photo: null
 }
 
+let products = []
+let cartlist = []
 
 window.onload = function () {
     //localStorage.clear()
 
     setcartvalue()
     showproductincart()
-   
-
-    for (let i = 1; i <= cartcount; i++) {
-        if (localStorage.getItem(`product${i}`)) {
-            document.getElementById(`prodaddcart${i}`).style.display = "none"
-            document.getElementById(`prodremovecart${i}`).style.display = "block"
+    setproductlist();
+    for (let i = 0; i < cartlist.length; i++) {
+        if (localStorage.getItem(`product${cartlist[i]}`)) {
+            document.getElementById(`prodaddcart${cartlist[i]}`).style.display = "none"
+            document.getElementById(`prodremovecart${cartlist[i]}`).style.display = "block"
         }
     }
 }
 
+function setproductlist() {
+    products = []
+    let str = JSON.parse(localStorage.getItem("productlist"));
 
-function addtocart(productid) {
-    cartcount++;
-    document.getElementById(`prodaddcart${productid}`).style.display = "none"
-    document.getElementById(`prodremovecart${productid}`).style.display = "block"
-    localStorage.setItem("cartcount", cartcount);
-    setcartvalue();
-    console.log(cartcount);
-    addproducttocart(productid)
-    showproductincart()
-
+    for (key in str) {
+        let value = str[key]
+        products[key] = value
+    }
 }
 
-function removefromcart(productid) {
-    cartcount--;
-    document.getElementById(`prodaddcart${productid}`).style.display = "block"
-    document.getElementById(`prodremovecart${productid}`).style.display = "none"
-    localStorage.setItem("cartcount", cartcount);
-    setcartvalue();
-    console.log(cartcount);
-    localStorage.removeItem(`product${productid}`);
+function updatecartlist() {
+    cartlist = []
+    let str = JSON.parse(localStorage.getItem("productlist"));
+
+    for (key in str) {
+        let value = str[key]
+        cartlist[key] = value
+    }
+
+    console.log(cartlist.length)
+}
+
+function setcartvalue() {
+    updatecartlist()
+    localStorage.setItem("cartcount", cartlist.length);
+    if (localStorage.getItem("cartcount")) {
+        cartvalue.innerText = localStorage.getItem("cartcount");
+
+    } else {
+        cartvalue.innerText = 0;
+    }
+}
+
+function addtocart(productid) {
+
+    document.getElementById(`prodaddcart${productid}`).style.display = "none"
+    document.getElementById(`prodremovecart${productid}`).style.display = "block"
+    addproducttocart(productid)
     showproductincart()
+    setcartvalue();
+
 }
 
 function addproducttocart(productid) {
@@ -54,26 +74,38 @@ function addproducttocart(productid) {
     product.price = document.getElementById(`prodprice${productid}`).innerText;
     product.photo = document.getElementById(`prodimg${productid}`).src;
     product.description = document.getElementById(`proddescrption${productid}`).innerText;
+    products.push(productid)
+    localStorage.setItem(`productlist`, JSON.stringify(products))
     localStorage.setItem(`product${productid}`, JSON.stringify(product))
+
 }
 
-function setcartvalue() {
-    if (localStorage.getItem("cartcount")) {
-        cartvalue.innerText = localStorage.getItem("cartcount");
-        console.log(cartcount);
-    } else {
-        cartvalue.innerText = 0;
-    }
+function removefromcart(productid) {
+
+    document.getElementById(`prodaddcart${productid}`).style.display = "block"
+    document.getElementById(`prodremovecart${productid}`).style.display = "none"
+    products = products.filter(item => item !== productid);
+    localStorage.setItem(`productlist`, JSON.stringify(products))
+    localStorage.removeItem(`product${productid}`);
+    removeproductfromcart(productid)
+    showproductincart()
+    setcartvalue();
 }
+
+
+
+
 
 function showproductincart() {
-    for (let i = 1; i <= 6; i++) {
-        if (JSON.parse(localStorage.getItem(`product${i}`))) {
-            if(document.getElementById(`cartitem${i}`)){
-                continue;
-            }
-            let cartproduct = JSON.parse(localStorage.getItem(`product${i}`));
-            let card = `    <div class="card"  id="cartitem${1}">
+
+    updatecartlist();
+
+    for (let i = 0; i < cartlist.length; i++) {
+        if (document.getElementById(`cartitem${cartlist[i]}`)) {
+            continue;
+        }
+        let cartproduct = JSON.parse(localStorage.getItem(`product${cartlist[i]}`));
+        let card = `    <div class="card"  id="cartitem${cartlist[i]}">
             <img src="${cartproduct.photo}" class="card-img-top" alt="..." id="prodimg${i}">
             <div class="card-body">
                 <h5 class="card-title" id="prodhead${i}"> ${cartproduct.name} </h5>
@@ -81,19 +113,24 @@ function showproductincart() {
                 </p>
                 <span class="card-text mt-2">Price : <span class="fw-bolder text-success" id="prodprice${i}">${cartproduct.price}</span>/
                 </span>
-            
-                
+
+
             </div>
         </div>`
-            document.getElementById("cartitems").innerHTML += card;
-        } else {
+        document.getElementById("cartitems").innerHTML += card;
+    }
+}
 
-            continue;
+function removeproductfromcart(productid) {
+    for (let i = 0; i < cartlist.length; i++) {
+        if (productid === cartlist[i]) {
+            document.getElementById(`cartitem${cartlist[i]}`).remove();
         }
     }
-
-
 }
+
+
+
 
 
 
